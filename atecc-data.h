4b3c718d797b6f369c37354feb3c5f443e40b4dd
Nikita webconn Maslov <wkernelteam@gmail.com>
2018-04-04 10:34:49 +0300

    atecc-util: add read-counter command
    Signed-off-by: Nikita webconn Maslov <wkernelteam@gmail.com>

--- atecc-data.h
+++ atecc-data.h
@@ -8,3 +8,6 @@ void atecc_read_data_help(const char *cmdname);
 
 int do_atecc_lock_data(int argc, char **argv);
 void atecc_lock_data_help(const char *cmdname);
+
+int do_atecc_read_counter(int argc, char **argv);
+void atecc_read_counter_help(const char *cmdname);
