3c1c2edb690a4958c897d4b832bd60109e08020e
Nikita webconn Maslov <wkernelteam@gmail.com>
2018-04-10 19:48:20 +0300

    atecc-util: add encrypted write for HMAC keys
    Signed-off-by: Nikita webconn Maslov <wkernelteam@gmail.com>

--- atecc-hmac.c
+++ atecc-hmac.c
@@ -8,14 +8,39 @@
 
 int do_atecc_hmac_write_key(int argc, char **argv)
 {
-    if (argc < 3) {
+    if (argc < 3 || argc == 4) {
         atecc_hmac_write_key_help(argv[0]);
         return 1;
     }
 
     int slot_id = atoi(argv[1]);
     const char *keyfilename = argv[2];
+    const char *writekeyfilename = NULL;
+    uint16_t key_id = 0;
+
+    if (argc == 5) {
+        writekeyfilename = argv[3];
+        key_id = atoi(argv[4]);
+    }
     size_t readlen = 0;
+
+    uint8_t writekey[HMAC_KEY_SIZE];
+    if (writekeyfilename) {
+        FILE *writekeyfile = fopen(writekeyfilename, "rb");
+        if (!writekeyfile) {
+            perror("open writekey file");
+            return 1;
+        }
+
+        readlen = fread(writekey, 1, sizeof (writekey), writekeyfile);
+        if (readlen != sizeof (writekey)) {
+            perror("read write key from file");
+            fclose(writekeyfile);
+            return 1;
+        }
+
+        fclose(writekeyfile);
+    }
 
     FILE *keyfile = maybe_fopen(keyfilename, "rb");
     if (!keyfile) {
@@ -31,28 +56,33 @@ int do_atecc_hmac_write_key(int argc, char **argv)
         return 1;
     }
 
-    if (!feof(keyfile)) {
-        eprintf("Keyfile length is more than %lu, abort\n", sizeof (key));
-        maybe_fclose(keyfile);
-        return 1;
-    }
-
-    ATCA_STATUS status = atcab_write_zone(ATCA_ZONE_DATA, slot_id, 0, 0, key, 32);
-    if (status != ATCA_SUCCESS) {
-        eprintf("Command atcab_write_zone is failed with status 0x%x\n", status);
-        return 2;
-    }
-
     maybe_fclose(keyfile);
+
+    ATCA_STATUS status;
+    if (!writekeyfilename) {
+        status = atcab_write_zone(ATCA_ZONE_DATA, slot_id, 0, 0, key, 32);
+        if (status != ATCA_SUCCESS) {
+            eprintf("Command atcab_write_zone is failed with status 0x%x\n", status);
+            return 2;
+        }
+    } else {
+        status = atcab_write_enc(slot_id, 0, key, writekey, key_id);
+        if (status != ATCA_SUCCESS) {
+            eprintf("Command atcab_write_enc is failed with status 0x%x\n", status);
+            return 2;
+        }
+    }
 
     return 0;
 }
 
 void atecc_hmac_write_key_help(const char *cmdname)
 {
-    eprintf("Usage: %s <slot_id> <keyfile>\n", cmdname);
+    eprintf("Usage: %s <slot_id> keyfile [write_key <write_key_id>]\n", cmdname);
     eprintf("\tslot_id\tID of slot to write HMAC key to\n");
     eprintf("\tkeyfile\tFile containing HMAC key to write (32 bytes long)\n");
+    eprintf("\twrite_key\tFile containing write-guarding key (32 bytes long)\n");
+    eprintf("\twrite_key_id\tID of write key on device\n");
 }
 
 int do_atecc_hmac_dgst(int argc, char **argv)
